a51e0b848c4e959ac1c4f38be972a6c29f99a595
Khalis Fadil <khalis_muse@yahoo.com>
2025-06-13 14:29:12 +0200

    add more function navMath

--- include/navMath.hpp
+++ include/navMath.hpp
@@ -17,7 +17,16 @@ namespace navMath {
             // Vector version: Convert multiple LLA coordinates to NED
             Eigen::MatrixXd LLA2NED(const Eigen::VectorXd& lat, const Eigen::VectorXd& lon, const Eigen::VectorXd& alt,
                                     double originlat, double originlon, double originalt);
+            
+            // Computes a normalized quaternion from roll, pitch, and yaw angles.
+            Eigen::Vector4d getQuat(double roll, double pitch, double yaw);
 
+            // Calculates the rotation matrix for a given unit quaternion.
+            //q Normalized unit quaternion as [qw, qx, qy, qz] (b-frame to n-frame).
+            Eigen::Matrix3d Cb2n(const Eigen::Vector4d& q);
+
+            // Computes a 4x4 homogeneous transformation matrix from a state vector.
+            Eigen::Matrix4d TransformMatrix(const Eigen::VectorXd& x);
         private:
 
             // Scalar SymmetricalAngle: Normalize angle to [-π, +π)
@@ -29,14 +38,5 @@ namespace navMath {
             //Safe normalization of an input vector.
             static Eigen::VectorXd Normalize(const Eigen::VectorXd& u);
             
-            // Computes a normalized quaternion from roll, pitch, and yaw angles.
-            static Eigen::Vector4d getQuat(double roll, double pitch, double yaw);
-            
-            // Calculates the rotation matrix for a given unit quaternion.
-            //q Normalized unit quaternion as [qw, qx, qy, qz] (b-frame to n-frame).
-            static Eigen::Matrix3d Cb2n(const Eigen::Vector4d& q);
-
-            // Computes a 4x4 homogeneous transformation matrix from a state vector.
-            static Eigen::Matrix4d TransformMatrix(const Eigen::VectorXd& x);
     };
 } // namespace navMath
